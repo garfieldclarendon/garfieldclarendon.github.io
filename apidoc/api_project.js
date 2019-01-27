60560e4a879d46bea836585ae01f6eb10b7ab472
jreilly434 <jr@boystownradio.com>
2019-01-27 14:17:16 -0600

    One more try

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-27T20:13:41.644Z",
+    "time": "2019-01-27T20:16:55.836Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
